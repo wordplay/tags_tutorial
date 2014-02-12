8137f4f0e1095c22f045ac2d56af5a3778dd83ec
Adam Horne <ahorne@wordplaysoftware.com>
2014-02-12 17:23:54 -0500

    Added tags to the Post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,6 +53,6 @@ class PostsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :tag_list)
     end
 end
